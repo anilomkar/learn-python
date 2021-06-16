100c4486396247c6f446577932f3281b739bb4dc
Anil Omkar <anilkumar.omkar@gmail.com>
2021-06-16 21:27:50 +0530

    Adding flask rest api first GET service

---
+++ app/flaskApi/api.py
@@ -0,0 +1,11 @@
+import flask
+import socket
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Welcome to the world of Python!</h1><p>Running from server - " + socket.gethostname() + "</p>"
+
+app.run()
